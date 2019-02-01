0f34e92e2dc01017f873c2af1fb39d6a9e09504b
yibuyisheng <yibuyisheng@163.com>
2019-02-01 21:44:09 +0800

    feat: 拆分模块

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+/**
+ * 获取小程序实例
+ */
+declare function getApp(): IApp;
+
+
+/**
+ * getCurrentPages() 函数用于获取当前页面栈的实例，
+ * 以数组形式按栈的顺序给出，第一个元素为首页，最后一个元素为当前页面。
+ */
+declare function getCurrentPages(): IPage[];
