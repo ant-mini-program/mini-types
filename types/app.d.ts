4d0c85982b284090c07cd2709cb172359a5452a0
yibuyisheng <yibuyisheng@163.com>
2019-02-17 20:51:39 +0800

    feat: 完善

--- types/app.d.ts
+++ types/app.d.ts
@@ -1,32 +1,55 @@
-/**
- * App 实现的接口对象
- * 参考: https://docs.alipay.com/mini/framework/app
- */
-declare interface IApp {
-  /**
-   * 生命周期函数--监听小程序初始化。当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
-   */
-  onLaunch?: (params?: my.IShowOptions) => void;
+declare namespace tinyapp {
+  type Query = Record<string, string | number>;
+
+  interface IAppLaunchOption {
+    query?: Query;
+    path?: string;
+  }
+
+  interface IAppOptionMethods {
+    /**
+     * 生命周期函数--监听小程序初始化。当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+     */
+    onLaunch?(option: IAppLaunchOption): void;
+
+    /**
+     * 生命周期函数--监听小程序显示。当小程序启动，或从后台进入前台显示，会触发 onShow
+     */
+    onShow?(option: IAppLaunchOption): void;
+
+    /**
+     * 生命周期函数--监听小程序隐藏。当小程序从前台进入后台，会触发 onHide
+     */
+    onHide?(): void;
+
+    /**
+     * 错误监听函数--当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+     */
+    onError?(): void;
+  }
+
+  interface IAppInstance<G> {
+    globalData: G;
+  }
 
   /**
-   * 生命周期函数--监听小程序显示。当小程序启动，或从后台进入前台显示，会触发 onShow
+   * App 实现的接口对象
+   * 参考: https://docs.alipay.com/mini/framework/app
    */
-  onShow?: (params?: my.IShowOptions) => void;
-
-  /**
-   * 生命周期函数--监听小程序隐藏。当小程序从前台进入后台，会触发 onHide
-   */
-  onHide?: () => void;
-
-  /**
-   * 错误监听函数--当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
-   */
-  onError?: (msg: string) => void;
-
-  globalData?: my.IData;
+  type AppOption<G> = IAppOptionMethods
+    & {
+        globalData?: G;
+        [name: string]: any;
+      }
+    & ThisType<IAppInstance<G>>;
 }
 
 /**
  * App() 函数用来注册一个小程序。接受一个 object 参数，其指定小程序的生命周期函数等。
  */
-declare function App(app: IApp): void;
+declare function App(option: tinyapp.AppOption<any>): void;
+
+/**
+ * 获取小程序实例
+ */
+declare function getApp(): { globalData: any };
--- types/component.d.ts
+++ types/component.d.ts
@@ -1,3 +1,28 @@
-declare interface IComponent extends IPage {}
+declare namespace tinyapp {
+  interface IComponentLifeCycleMethods {
+    didMount?(): void;
+    didUpdate?(): void;
+    didUnmount?(): void;
+  }
 
-declare function Component(component: IComponent): void;
+  interface IComponentMethods {
+    [name: string]: () => void;
+  }
+
+  interface IComponentInstance<P, D> {
+    data: D;
+    props: P;
+    setData: SetDataMethod<D>;
+  }
+
+  type ComponentOption<P extends Record<string, any>, D extends any, M extends IComponentMethods> = IComponentLifeCycleMethods
+    & {
+      mixins?: Array<ComponentOption<any, any, any>>;
+      data?: D;
+      props?: P;
+      methods?: M & ThisType<IComponentInstance<P, D> & M>;
+    }
+    & ThisType<IComponentInstance<P, D> & M>;
+}
+
+declare function Component(option: tinyapp.ComponentOption<Record<string, any>, any, tinyapp.IComponentMethods>): void;
