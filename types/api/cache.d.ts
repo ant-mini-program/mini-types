e84a155dae2680063dcb81ef69c6bd9a4a7b6bde
yibuyisheng <yibuyisheng@163.com>
2019-02-25 13:03:26 +0800

    feat: 缓存

---
+++ types/api/cache.d.ts
@@ -0,0 +1,61 @@
+/**
+ * @file 缓存
+ */
+declare namespace my {
+  interface ISetStorageOptions {
+    key?: string;
+    data?: string | Record<string, any>;
+    success?(): void;
+    fail?(): void;
+    complete?(): void;
+  }
+
+  function setStorage(options: ISetStorageOptions): void;
+
+  interface ISetStorageSyncOptions {
+    key: string;
+    data: string | Record<string, any>;
+  }
+
+  function setStorageSync(options: ISetStorageSyncOptions): void;
+
+  interface IGetStorageOptions {
+    key: string;
+    success?(res: { data: string | Record<string, any>; }): void;
+    fail?(): void;
+    complete?(): void;
+  }
+
+  function getStorage(option: IGetStorageOptions): void;
+
+  function getStorageSync(options: { key: string; }): void;
+
+  interface IRemoveStorageOptions {
+    key: string;
+    success?(): void;
+    fail?(): void;
+    complete?(): void;
+  }
+
+  function removeStorage(options: IRemoveStorageOptions): void;
+
+  function removeStorageSync(options: { key: string; }): void;
+
+  function clearStorage(): void;
+
+  function clearStorageSync(): void;
+
+  interface IStorageInfo {
+    keys: string[];
+    currentSize: string;
+    limitSize: string;
+  }
+
+  function getStorageInfo(options: {
+    success?(res: IStorageInfo): void;
+    fail?(): void;
+    complete?(): void;
+  }): void;
+
+  function getStorageInfoSync(): IStorageInfo;
+}
