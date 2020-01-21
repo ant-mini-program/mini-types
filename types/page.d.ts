00d693c43feaa3f661d69d722968a613e10fe0a0
Wei Zhu <yesmeck@gmail.com>
2020-01-21 13:54:13 +0800

    refactor: 把全局变量申明放入单独的文件里

--- types/page.d.ts
+++ types/page.d.ts
@@ -155,17 +155,3 @@ declare namespace tinyapp {
       }
     & ThisType<IPageInstance<D>>;
 }
-
-/**
- * Page() 函数用来注册一个页面。
- * 接受一个 object 参数，其指定页面的初始数据、生命周期函数、事件处理函数等。
- */
-/* tslint:disable:no-unnecessary-generics */
-declare function Page<D>(options: tinyapp.PageOptions<D>): void;
-/* tslint:enable:no-unnecessary-generics */
-
-/**
- * getCurrentPages() 函数用于获取当前页面栈的实例，
- * 以数组形式按栈的顺序给出，第一个元素为首页，最后一个元素为当前页面。
- */
-declare function getCurrentPages(): Array<tinyapp.IPageInstance<any>>;
