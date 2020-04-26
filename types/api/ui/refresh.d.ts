d83db9c4e2244b03a9bba7ff591bf9ccae85d799
guoshencheng <648772021@qq.com>
2020-04-26 12:55:54 +0800

    feat: fix clipboard type and some interface name spell problem

--- types/api/ui/refresh.d.ts
+++ types/api/ui/refresh.d.ts
@@ -2,12 +2,12 @@
  * @file 下拉刷新
  */
 declare namespace my {
-  interface IStartPullDownRefresh {
+  interface IStartPullDownRefreshOptions {
     success?: () => void;
     fail?: () => void;
     complete?: () => void;
   }
-  interface IStopPullDownRefresh {
+  interface IStopPullDownRefreshOptions {
     success?: () => void;
     fail?: () => void;
     complete?: () => void;
@@ -15,10 +15,10 @@ declare namespace my {
   /**
    * 停止当前页面的下拉刷新。
    */
-  function stopPullDownRefresh(options?: IStopPullDownRefresh): void;
+  function stopPullDownRefresh(options?: IStartPullDownRefreshOptions): void;
 
   /**
    * 开始下拉刷新。代码执行后触发下拉刷新动画，效果与用户手动下拉刷新保持一致。
    */
-  function startPullDownRefresh(options?: IStartPullDownRefresh): void;
+  function startPullDownRefresh(options?: IStopPullDownRefreshOptions): void;
 }
