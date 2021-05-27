460c0e1c044a491ff59b772813a03ca8b1f02eff
xlfsummer <20750969+xlfsummer@users.noreply.github.com>
2021-05-27 20:40:08 +0800

    fix: 修复缺少泛型参数

--- types/global.d.ts
+++ types/global.d.ts
@@ -22,7 +22,7 @@ declare function Page<D, M>(options: tinyapp.PageOptions<D, M>): void;
  * getCurrentPages() 函数用于获取当前页面栈的实例，
  * 以数组形式按栈的顺序给出，第一个元素为首页，最后一个元素为当前页面。
  */
-declare function getCurrentPages(): Array<tinyapp.IPageInstance<any>>;
+declare function getCurrentPages(): Array<tinyapp.IPageInstance<any, any>>;
 
 /* tslint:disable:no-unnecessary-generics */
 declare function Component<P, D, M extends tinyapp.IComponentMethods>(
